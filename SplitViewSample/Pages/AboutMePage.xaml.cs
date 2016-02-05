c8ad0f694385d9865431ebbff68dd5c98fe122c7
Oleg Samoylov <quillaur@outlook.com>
2016-02-05 23:36:24 +1000

    Added new files.

---
+++ SplitViewSample/Pages/AboutMePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SplitViewSample.Pages
+{
+    /// <summary>
+    /// Второстепенная страница.
+    /// </summary>
+    public sealed partial class AboutMePage : Page
+    {
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public AboutMePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("ms-windows-store:publisher?name=Oleg%20Samoylov"));
+        }
+    }
+}
---
+++ SplitViewSample/Pages/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SplitViewSample.Pages
+{
+    /// <summary>
+    /// Домашняя страница.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
